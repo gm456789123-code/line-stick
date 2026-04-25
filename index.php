909a1d474ab5848a7142334876fb15f1bed8750f
gm456789123-code <gm456789123@gmail.com>
2026-04-25 18:21:44 +0700

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-// Root entrypoint: send users to the frontend home page.
-$frontendUrl = getenv('FRONTEND_HOME_URL') ?: 'http://localhost:3000/';
-
-header('Location: ' . $frontendUrl, true, 302);
-exit;
